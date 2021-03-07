3b331088b0a9cddb8912a409c1552a0330a6d985
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-07 16:34:05 +0100

    Express server configured express server and get/post routes

---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static('dist'))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const cors = require('cors')
+app.use(cors())
+
+const port = 8081
+app.listen(port, () => { console.log(`The server is running on port ${port}`)})
+console.log(__dirname);
+const fetchedData = {}
+// GET route 
+app.get('/all', (res, req) => res.send(fetchedData))
+
+// POST route
+app.post('/', postData)
+function postData(req, res) {
+    fetchedData = req.body
+    res.send(fetchedData)
+    console.log(fetchedData);
+}

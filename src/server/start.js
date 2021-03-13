11d0849cc688558807c2a9669ba03c4288926eea
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-13 22:37:27 +0100

    Changed port heroku

--- src/server/start.js
+++ src/server/start.js
@@ -1,5 +1,5 @@
 const app = require('./server')
 
-const port = process.env.PORT || 8080
+const port = 46736
 app.listen(port, () => { console.log(`The server is running on port ${port}`) })
 console.log(__dirname)
